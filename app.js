9db920c8fc3ea77dba0f756133c80a5f4774a1b8
Grego <gregovilardo@gmail.com>
2021-07-15 10:29:19 -0300

    put auth2.0

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const port = 3000;
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+//find or create is a mongoose package that gives functionality to the findOrCreate function of passport.js Oauth20
+//the original is mongoose-findorcreate but it seems that does not have mainteing
+const findOrCreate = require("mongoose-find-or-create");
 
 app.use(express.urlencoded({ enabled: true }));
 app.set("view engine", "ejs");
@@ -33,20 +37,86 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
+    googleId: String,
+    secret: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/google/secrets",
+            userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+        },
+        function (accessToken, refreshToken, profile, cb) {
+            console.log(profile);
+            User.findOrCreate({ googleId: profile.id }, function (err, user) {
+                return cb(err, user);
+            });
+        }
+    )
+);
 
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get("/submit", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.render("login");
+    }
+});
+
+app.post("/submit", (req, res) => {
+    const secret = req.body.secret;
+    console.log(req.user.id);
+    User.findById(req.user.id, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = secret;
+                foundUser.save(() => {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+});
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+    "/auth/google/secrets",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect("/secrets");
+    }
+);
 
 app.route("/login")
 
@@ -94,11 +164,15 @@ app.route("/register")
     });
 
 app.get("/secrets", (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render("secrets");
-    } else {
-        res.redirect("/login");
-    }
+    User.find({"secret": {$ne: null}}, (err, foundUser) =>{
+        if (err) {
+            console.log(err)
+        } else {
+            if (foundUser) {
+                res.render("secrets", { userWithSecrets: foundUser })
+            }
+        }
+    })
 });
 
 app.get("/logout", (req, res) => {
